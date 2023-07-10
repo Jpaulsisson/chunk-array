6e441ab7e64f776992b3bf4d565dab11c8f8eb13
Paul Sisson <paulsissonsemail@gmail.com>
2023-07-09 20:42:23 -0500

    I'm using this to practice writing a really long commit message

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const testArray = [1,2,3,4,5,6,7,8,9,0];
 const testArray2 = [];
 
+
+//// first attempt ///// 
 function chunker(array, size) {
   if (array.length < 1) {
     return [];
@@ -8,7 +10,7 @@ function chunker(array, size) {
   let result = [];
   let i= 0;
   do {
-    const chunk = array.slice(i, size + i);
+    const chunk = array.slice(i, size + i); ///picked an easy one since I haven't done an algorithm solve in a while
     result.push(chunk);
     i = i + size;
   } while (i < array.length)
@@ -16,3 +18,6 @@ function chunker(array, size) {
 }
 
 console.log(chunker(testArray2, 3))
+
+//// solved //////
+/// Pretty simple here and looks like most everyone else came to the same conclusion ///
